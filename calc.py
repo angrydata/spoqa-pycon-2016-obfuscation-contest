ca017233a9cd0266a98262acea4fed663e98c252
Sumin Byeon <suminb@gmail.com>
2016-08-13 23:14:23 +0900

    Getting started

--- calc.py
+++ calc.py
@@ -1,2 +1,21 @@
+import subprocess
 import sys
-print(eval(sys.argv[1]))
+
+
+def main(expr):
+    stmt = 'print({})'.format(expr)
+    p = subprocess.Popen(['python3', '-c', stmt],
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    result, _ = p.communicate()
+    return result.decode('utf-8').strip()
+
+
+if __name__ == '__main__':
+    x = main(sys.argv[1])
+    try:
+        float(x)
+    except ValueError:
+        sys.exit(1)
+    else:
+        print(x)
