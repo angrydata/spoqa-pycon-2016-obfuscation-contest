37cedf9deeeb6fe6ad2c45447e1232027f983ece
Sumin Byeon <suminb@gmail.com>
2016-08-14 00:58:15 +0900

    Replace all built-in functions

--- calc.py
+++ calc.py
@@ -1,20 +1,21 @@
 base, space = 1 << 8, 1 << 5
+_, __ = getattr, globals
+builtins = lambda x: _(__()['__builtins__'], x)
 modules = [
     (lambda _, __, ___: _(_, __, ___))(
         lambda _, __, ___:
-            chr(___ % __) + _(_, __, ___ // __) if ___ else
-            chr(space)[:-1],
+            builtins('chr')(___ % __) + _(_, __, ___ // __) if ___ else
+            builtins('chr')(space)[:-1],
         base,
         545200826904043625543027),
     (lambda _, __, ___: _(_, __, ___))(
         lambda _, __, ___:
-            chr(___ % __) + _(_, __, ___ // __) if ___ else
-            chr(space)[:-1],
+            builtins('chr')(___ % __) + _(_, __, ___ // __) if ___ else
+            builtins('chr')(space)[:-1],
         base,
         7567731),
 ]
-subprocess, sys = map(__import__, modules)
-_, __ = getattr, globals
+subprocess, sys = builtins('map')(builtins('__import__'), modules)
 
 expr = _(sys, 'argv')[1]
 popen = _(subprocess, 'Popen')
@@ -25,8 +26,8 @@ result, ___ = _(popen(args, **kwargs), 'communicate')()
 x = _(_(result, 'decode')('utf-8'), 'strip')()
 
 try:
-    _(__()['__builtins__'], 'float')(x)
+    builtins('float')(x)
 except:
     _(sys, 'exit')(1)
 else:
-    _(__()['__builtins__'], 'print')(x)
+    builtins('print')(x)
