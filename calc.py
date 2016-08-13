667cb127b6468a682c803d3e49a4692fb76b30b9
Sumin Byeon <suminb@gmail.com>
2016-08-14 01:03:14 +0900

    Substitude decoder with a variable L

--- calc.py
+++ calc.py
@@ -1,21 +1,16 @@
-base, space = 1 << 8, 1 << 5
+B, space = 1 << 8, 1 << 5
 _, __ = getattr, globals
 builtins = lambda x: _(__()['__builtins__'], x)
+L = lambda _, __, ___: \
+    builtins('chr')(___ % __) + _(_, __, ___ // __) if ___ else \
+    builtins('chr')(space)[:-1]
 modules = [
-    (lambda _, __, ___: _(_, __, ___))(
-        lambda _, __, ___:
-            builtins('chr')(___ % __) + _(_, __, ___ // __) if ___ else
-            builtins('chr')(space)[:-1],
-        base,
-        545200826904043625543027),
-    (lambda _, __, ___: _(_, __, ___))(
-        lambda _, __, ___:
-            builtins('chr')(___ % __) + _(_, __, ___ // __) if ___ else
-            builtins('chr')(space)[:-1],
-        base,
-        7567731),
+    (lambda _, __, ___: _(_, __, ___))(L, B, 545200826904043625543027),
+    (lambda _, __, ___: _(_, __, ___))(L, B, 7567731),
 ]
-subprocess, sys = builtins('map')(builtins('__import__'), modules)
+subprocess, sys = builtins(
+    (lambda _, __, ___: _(_, __, ___))(L, B, 7364973),
+)(builtins('__import__'), modules)
 
 expr = _(sys, 'argv')[1]
 popen = _(subprocess, 'Popen')
